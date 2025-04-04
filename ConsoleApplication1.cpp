dd1e0300550a991210da049367db2b33d995c269
yassin2007us <139810608+yassin2007us@users.noreply.github.com>
2025-04-04 20:25:01 +0200

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+int main()
+//{
+//	int x, y, u;
+//	cin >> x>> y>> u;
+//	int rows = (x + u - 1) / u;
+//	int cols = (y + u - 1) / u;
+//	cout << rows * cols;
+//}
+
+//{
+//	int a, s, d;
+//	cin >> a >> s >> d;
+//	if (a + s + d >= 2)
+//	{
+//		cout << 1;
+//	}
+//	else
+//	{
+//		cout << 0;
+//	}
+//}
+
+//{
+//	int temprature = 0;
+//	string change;
+//	int n;
+//	cout << "how many tomes do u wanty to change the temprature:";
+//	cin >> n;
+//
+//	for (int i = 0;i < n;i++)
+//	{
+//		cin >> change;
+//		if (change[0] == '+' || change[1] == '1')
+//		{
+//			temprature++;
+//			cout << temprature << "\n";
+//		}
+//		else
+//		{
+//			temprature--;
+//			cout << temprature << "\n";
+//		}
+//
+//	}
+//	}
+
+//{
+//
+//}
+
+//{
+//}
+
